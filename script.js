35cb534f169dbd711358f92b64a2a5ac6cee1f7b
harsh6453 <harsh28082003@gmail.com>
2024-06-24 21:22:50 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let a = document.getElementById("Login1");
+let b = document.getElementById("Register");
+let c = document.getElementById("btn");
+function register(){
+    a.style.left = "-450px";
+    b.style.left = "50px";
+    c.style.left = "120px";
+}
+function login(){
+    a.style.left = "50px";
+    b.style.left = "500px";
+    c.style.left = "0px";
+}
